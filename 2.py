baf0161676eec862bd9a31478101dd36f07564fa
Andrii Havrik <aohavrik@gmail.com>
2024-10-29 20:55:45 +0200

    ls2

---
+++ 2.py
@@ -0,0 +1,24 @@
+#2
+print("    \"Anyone who")
+print("          stops")
+print("                learning is old,")
+print("                     whether at twenty or eighty\".")
+print("                           Henry Ford")
+#3
+
+num1 = int(input("Введіть перше число: "))
+num2 = int(input("Введіть друге число: "))
+
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+
+print(f"Сума: {sum_result} Різниця: {difference_result} Добуток: {product_result}")
+
+#3
+width = int(input("Введіть ширину прямокутника: "))
+height = int(input("Введіть висоту прямокутника: "))
+
+area = width * height
+
+print(f"Площа прямокутника: {area}")
